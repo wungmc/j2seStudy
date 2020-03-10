c66db6253937c15707982fe50634d8bd6c051edc
wung <wungcc@gmail.com>
2020-03-10 11:13:53 +0800

    Thread.join 测试

--- src/main/java/com/wung/thread/MyThread.java
+++ src/main/java/com/wung/thread/MyThread.java
@@ -13,7 +13,7 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
             System.out.println(name + ", i = " + i);
         }
     }
